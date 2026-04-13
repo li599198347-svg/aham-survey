73b8fe4c050c21f37f0d037d7811f2218c4133f8
lichengbao <li599198347@gmai.com>
2026-04-13 12:38:05 +0800

    Initial Commit

---
+++ Aham/AhamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhamApp.swift
+//  Aham
+//
+//  Created by 李成豹 on 2026/4/13.
+//
+
+import SwiftUI
+
+@main
+struct AhamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aham/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aham
+//
+//  Created by 李成豹 on 2026/4/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
